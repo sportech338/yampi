74101dcf7962d016fd5235f485870e4f08802003
sportech338 <sportechofc01@gmail.com>
2025-03-21 10:40:48 -0300

    Update sync_carts.py

--- sync_carts.py
+++ sync_carts.py
@@ -1,5 +1,6 @@
 import requests
-import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import os
 
 # CONFIGURAÇÕES
@@ -27,3 +28,24 @@ if response.status_code == 200:
 else:
     print("Erro ao buscar carrinhos:", response.status_code)
     print(response.text)
+
+# Autenticar com o Google Sheets
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+credentials = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(credentials)
+
+# Conectar à planilha com o ID
+SPREADSHEET_ID = '1OBKs2RpmRNqHDn6xE3uMOU-bwwnO_JY1ZhqctZGpA3E'
+spreadsheet = client.open_by_key(SPREADSHEET_ID)
+sheet = spreadsheet.sheet1  # Selecionar a primeira aba da planilha
+
+# Inserir dados na planilha
+for cart in carts_data:
+    customer_name = cart['tracking_data']['name']
+    customer_email = cart['tracking_data']['email']
+    product_name = cart['items'][0]['sku']['data']['title']
+    total = cart['totalizers']['total']
+
+    # Adicionar uma nova linha com os dados dos carrinhos abandonados
+    sheet.append_row([customer_name, customer_email, product_name, total])
+    print(f"Carrinho de {customer_name} adicionado com sucesso.")
